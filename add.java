d992d3873064d79152a9c9973a451f00ee8a4a0d
Rishabh Pathak <pathakrishabh096@gmail.com>
2025-08-05 10:53:07 +0530

    Initial commit

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class add {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a+b);
+        System.out.println();
+        sc.close();
+    }
+}
